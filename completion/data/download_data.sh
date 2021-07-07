0906c6dd88b8a341564d49bf9ec929721d833c2e
paul007pl <panliang_de2007@qq.com>
2021-07-07 23:23:28 +0800

    update download links

--- completion/data/download_data.sh
+++ completion/data/download_data.sh
@@ -1,3 +1,3 @@
-wget https://www.dropbox.com/s/q8mik908xfabh5l/MVP_Train_CP.h5?dl=0
-wget https://www.dropbox.com/s/5ugvpknpp18k0qn/MVP_Test_CP.h5?dl=0
-wget https://www.dropbox.com/s/onl59s1wc7vizm1/MVP_ExtraTest_CP.h5?dl=0
+wget https://www.dropbox.com/s/q8mik908xfabh5l/MVP_Train_CP.h5
+wget https://www.dropbox.com/s/5ugvpknpp18k0qn/MVP_Test_CP.h5
+wget https://www.dropbox.com/s/onl59s1wc7vizm1/MVP_ExtraTest_CP.h5
--- registration/data/download_data.sh
+++ registration/data/download_data.sh
@@ -1,3 +1,3 @@
-wget https://www.dropbox.com/s/qvmxb1ivt6uu9pp/MVP_Train_RG.h5?dl=0
-wget https://www.dropbox.com/s/48xh681d2j5n3eb/MVP_Test_RG.h5?dl=0
-wget https://www.dropbox.com/s/stft48x1hy77isb/MVP_ExtraTest_RG.h5?dl=0
+wget https://www.dropbox.com/s/qvmxb1ivt6uu9pp/MVP_Train_RG.h5
+wget https://www.dropbox.com/s/48xh681d2j5n3eb/MVP_Test_RG.h5
+wget https://www.dropbox.com/s/stft48x1hy77isb/MVP_ExtraTest_RG.h5
