32011c1e0a513a9939e8962fd1d32ba2ea8c8f45
paul007pl <panliang_de2007@qq.com>
2021-07-07 11:33:48 +0800

    update links

--- registration/data/download_data.sh
+++ registration/data/download_data.sh
@@ -1,4 +1,3 @@
-wget https://www.dropbox.com/sh/la0kwlqx4n2s5e3/AADGsHD9wl0Quhha1CIY7bwYa/mvp_test_gt_2048pts.h5
-wget https://www.dropbox.com/sh/la0kwlqx4n2s5e3/AACuytSzxru8rgypU6WvBd8Oa/mvp_test_input.h5
-wget https://www.dropbox.com/sh/la0kwlqx4n2s5e3/AAAKS_UaCr-EUDP0MHwVufcOa/mvp_train_gt_2048pts.h5
-wget https://www.dropbox.com/sh/la0kwlqx4n2s5e3/AABJsqLDf-bmlF_vv3kV--Jya/mvp_train_input.h5
+wget https://www.dropbox.com/s/qvmxb1ivt6uu9pp/MVP_Train_RG.h5?dl=0
+wget https://www.dropbox.com/s/48xh681d2j5n3eb/MVP_Test_RG.h5?dl=0
+wget https://www.dropbox.com/s/stft48x1hy77isb/MVP_ExtraTest_RG.h5?dl=0
